401b82fd154ee1cf386adbaa48e9ac8cf96d46a6
lucasdclemos@gmail.com <lucasdclemos@gmail.com>
2023-12-11 23:25:15 -0300

    Um pouco da logica do jogo

--- projeto_final/main.lua
+++ projeto_final/main.lua
@@ -1,32 +1,62 @@
 --local mqtt = require("mqtt_library")
 
-local linha = 0
-local coluna = 0
-local matriz = {}
-local jogador = 1
+local jogador = 1 -- 1 (branco) - 0 (preto)
 local numLinhas = 8
 local numColunas = 8
 local tamanhoCasa = 64
+local destaque_x
+local destaque_y
+local destacado = 0
+local x = 0
+local y = 0
+local escolhendo_peca = 0
 -- Peça branca - 1
 -- Peça preta - 0
 -- Desocupado - -1
+tabuleiro = {}
 
 function controller(msg)
-  
+  if msg == "1" then
+    verifica_tabuleiro(destaque_x - 1, destaque_y - 1)
+  elseif msg == "2" then
+    verifica_tabuleiro(destaque_x - 1, destaque_y + 1)
+  elseif msg == "3" then
+    verifica_tabuleiro(destaque_x + 1, destaque_y - 1)
+  else
+    verifica_tabuleiro(destaque_x + 1, destaque_y + 1)
+  end
 end
+
+function posicao_confirmada() then
+  
+
 
 function mqttcb(t,m)
   if(t == "paraloveA14") then
+  
   end
   print("MENSAGEM RECEBIDA: "..m)
   controller(m)
+end
+
+function verifica_clique(x, y)
+  destaque_x = math.floor(x / tamanhoCasa) + 1
+  destaque_y = math.floor(y / tamanhoCasa) + 1
+  print(destaque_x, destaque_y)
+  if tabuleiro[destaque_x][destaque_y] == jogador then
+    destacado = 1
+  end
   
-    
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+  verifica_clique(x, y)
 end
 
 function love.load()
+    mouseX = 0
+    mouseY = 0
     love.window.setMode(numColunas * tamanhoCasa, numLinhas * tamanhoCasa)
-    tabuleiro = {}
     for linha = 1, numLinhas do
         tabuleiro[linha] = {}
         for coluna = 1, numColunas do
@@ -66,6 +96,33 @@ function love.draw()
                 love.graphics.circle("fill", x + tamanhoCasa / 2, y + tamanhoCasa / 2, tamanhoCasa / 2 - 5)
             end
         end
+    end
+    
+    if escolhendo_peca == 0 then
+      if jogador == 1 then
+        love.graphics.setColor(1, 1, 1)
+      elseif jogador == 0 then
+        love.graphics.setColor(0, 0, 0)
+      end
+      love.graphics.rectangle("line", (x - 1)*tamanhoCasa, (y - 1)*tamanhoCasa, tamanhoCasa, tamanhoCasa)
+    end
+    
+    if destacado == 1 then
+      if jogador == 1 then
+        love.graphics.setColor(1, 1, 1)
+      elseif jogador == 0 then
+        love.graphics.setColor(0, 0, 0)
+      end
+      love.graphics.rectangle("line", (destaque_x - 1)*tamanhoCasa, (destaque_y - 1)*tamanhoCasa, tamanhoCasa, tamanhoCasa)
+    end
+    
+    if escolhendo == 1 then
+      if jogador == 1 then
+        love.graphics.setColor(1, 1, 1)
+      elseif jogador == 0 then
+        love.graphics.setColor(0, 0, 0)
+      end
+      love.graphics.rectangle("line", (destaque_x - 1)*tamanhoCasa, (destaque_y - 1)*tamanhoCasa, tamanhoCasa, tamanhoCasa)
     end
 end
 
